e1d280c40f52e252638fafc6206911239d1ec0f0
shanigoren <shanigoren@campus.technion.ac.il>
2022-12-20 14:04:18 +0200

    scripts

--- image_classification/scripts/run_noteacher.sh
+++ image_classification/scripts/run_noteacher.sh
@@ -3,4 +3,7 @@
 source $HOME/miniconda3/etc/profile.d/conda.sh
 conda activate skd
 
-srun -c 2 --gres=gpu:1 python3 experiments/no_teacher.py -d imagewoof -m resnet34 -e 100 -s 1
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+srun -c 2 --gres=gpu:1 python3 experiments/no_teacher.py -d imagewoof -m resnet18 -e 100 -s 0
